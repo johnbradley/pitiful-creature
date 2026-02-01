a348fd64bc5cf055532cc555c336b580ac24e2e3
Jason Mark <jason@jasonondesign.com>
2026-01-31 20:42:39 -0500

    ability to get blue mask, but doesn't show up in inventory. also tweaked ground planes and such

---
+++ blue_mask_area_2d.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+const BLUE_MASK_AREA = "blue-mask"
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		GameManager.player_enter_area(BLUE_MASK_AREA)
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		GameManager.player_leave_area(BLUE_MASK_AREA)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		var keycode = event.keycode
+		if keycode == KEY_E:
+			if GameManager.player_in_area("blue-mask"):
+				GameManager.acquire_mask(GameManager.MASK_DAPPER)
+				get_parent().queue_free()
